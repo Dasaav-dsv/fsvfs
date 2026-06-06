49bb6b0bf8be1efc9be9c4c1e0c37f611822f99b
Dasaav-dsv <simplydasaav@gmail.com>
2026-06-06 20:31:34 +0200

    Remove erroneous space
    Signed-off-by: Dasaav-dsv <simplydasaav@gmail.com>

--- scripts/Mount-Ds3.ps1
+++ scripts/Mount-Ds3.ps1
@@ -1,4 +1,4 @@
-cargo run -- release -- dvdbnd -k dist/dvdbnd/Key -d dist/dvdbnd/Hash -m ../darksouls3 `
+cargo run --release -- dvdbnd -k dist/dvdbnd/Key -d dist/dvdbnd/Hash -m ../darksouls3 `
     "D:\Steam\steamapps\common\DARK SOULS III\Game\Data1.bhd" `
     "D:\Steam\steamapps\common\DARK SOULS III\Game\Data2.bhd" `
     "D:\Steam\steamapps\common\DARK SOULS III\Game\Data3.bhd" `
