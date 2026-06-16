3f1d6e5685be8c4d22f4c55107898859b0bbf118
Dasaav-dsv <simplydasaav@gmail.com>
2026-06-16 20:07:04 +0200

    Embed a manifest

--- build.rs
+++ build.rs
@@ -1,25 +1,51 @@
 fn main() {
-    #[cfg(windows)]
-    delayload_projectedfslib();
-
     println!("cargo::rerun-if-changed=build.rs");
+    os::main();
 }
 
 #[cfg(windows)]
-fn delayload_projectedfslib() {
-    let target_env =
-        std::env::var("CARGO_CFG_TARGET_ENV").expect("CARGO_CFG_TARGET_ENV must be set");
+mod os {
+    use std::env;
 
-    match target_env.as_str() {
-        "msvc" => {
-            println!("cargo:rustc-link-lib=dylib=delayimp");
-            println!("cargo:rustc-link-arg=/DELAYLOAD:projectedfslib.dll");
-        }
-        "gnu" => {
-            println!("cargo:rustc-link-arg=-Wl,--delayload=projectedfslib.dll");
-        }
-        env => {
-            panic!("unsupported target {env}");
+    use embed_manifest::{
+        manifest::{HeapType, SupportedOS},
+        new_manifest,
+    };
+
+    pub fn main() {
+        delayload_projectedfslib();
+        embed_manifest();
+    }
+
+    fn delayload_projectedfslib() {
+        let target_env =
+            env::var("CARGO_CFG_TARGET_ENV").expect("CARGO_CFG_TARGET_ENV must be set");
+
+        match target_env.as_str() {
+            "msvc" => {
+                println!("cargo:rustc-link-lib=dylib=delayimp");
+                println!("cargo:rustc-link-arg=/DELAYLOAD:projectedfslib.dll");
+            }
+            "gnu" => {
+                println!("cargo:rustc-link-arg=-Wl,--delayload=projectedfslib.dll");
+            }
+            env => {
+                panic!("unsupported target {env}");
+            }
         }
     }
+
+    fn embed_manifest() {
+        embed_manifest::embed_manifest(
+            new_manifest("fsvfs")
+                .supported_os(SupportedOS::Windows10..)
+                .heap_type(HeapType::SegmentHeap),
+        )
+        .expect("failed to embed manifest")
+    }
+}
+
+#[cfg(unix)]
+mod os {
+    pub fn main() {}
 }
