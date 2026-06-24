7e11b94d594abdebb916b511709e7fad4bd6f11c
Dasaav-dsv <simplydasaav@gmail.com>
2026-06-24 03:11:02 +0200

    Windows enhancements

--- gui/build.rs
+++ gui/build.rs
@@ -1,4 +1,18 @@
+#[cfg(windows)]
+use embed_manifest::{
+    manifest::{HeapType, SupportedOS},
+    new_manifest,
+};
+
 fn main() {
     println!("cargo::rerun-if-changed=ui/*");
     slint_build::compile("ui/app.slint").unwrap();
+
+    #[cfg(windows)]
+    embed_manifest::embed_manifest(
+        new_manifest("fsvfs-gui")
+            .supported_os(SupportedOS::Windows10..)
+            .heap_type(HeapType::SegmentHeap),
+    )
+    .expect("failed to embed manifest")
 }
